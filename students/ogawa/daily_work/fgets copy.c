be0c29e927e28a2829a810a06630d2fabd67912a
ogatatsulab <ogatatsulab@gmailcom>
2026-07-21 17:58:07 +0900

    20260721-1758

--- students/ogawa/daily_work/fgets copy.c
+++ students/ogawa/daily_work/fgets copy.c
@@ -7,18 +7,18 @@ int main( void ) {
 
   char string[ 5 ];
 
-  while ( 1 ) {
+  while ( 1 ) {     //1
 
     printf( "Input String => " );
     fgets( string, sizeof( string ), stdin );
 
     printf( "string(%d): %s\n", ++count, string );
 
-    if ( ! strchr( string, '\n' ) ) {
-      while( getchar() != '\n' );
-    }
+    if ( ! strchr( string, '\n' ) ) {    //3
+      while( getchar() != '\n' );     //2
+    }     //3
 
-   }
+  }     //1
 
   return 0;
 
