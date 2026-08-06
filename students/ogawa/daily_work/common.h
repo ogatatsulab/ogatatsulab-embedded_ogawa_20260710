8285077590fe3e949d62d84107e7b76f9d772d03
ogatatsulab <ogatatsulab@gmailcom>
2026-08-07 08:56:45 +0900

    202608070856

---
+++ students/ogawa/daily_work/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef struct {
+  char name[ 30 ];
+  int age;
+} Person;
+
+#endif
---
+++ students/ogawa/daily_work/include.c
@@ -0,0 +1,6 @@
+#include "include1.h"
+#include "include2.h"
+
+int main( void ) {
+  return 0;
+}
